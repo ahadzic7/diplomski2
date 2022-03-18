d5577576e2e2910a487f4f6c033ed70ef7925495
Armin <Hadzic>
2022-03-18 11:46:11 +0100

    commit

---
+++ vulnerable-application/src/main/java/com/example/log4shell/log4j.java
@@ -0,0 +1,13 @@
+package com.example.log4shell;
+
+
+import org.apache.logging.log4j.core.Logger;
+//import org.apache.logging.log4j.LogManager;
+
+//import org.apache.logging.log4j.LogManager;
+
+
+public class log4j {
+    //private static final Logger logger = LogManager.getLogger(log4j.class);
+
+}
